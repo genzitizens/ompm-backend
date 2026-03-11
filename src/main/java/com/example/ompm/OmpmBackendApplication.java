8e446870783490d54476bb9c6d9eb6610d3156f8
mediocreasian <exelbert2010@gmail.com>
2026-03-11 23:43:09 +0800

    Innit commit

---
+++ src/main/java/com/example/ompm/OmpmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ompm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmpmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmpmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ompm/OmpmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ompm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmpmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
